e857a7cf72f42c4e9d30020b226517adb04674a1
ortizdavid <ortizaad1994@gmail.com>
2023-11-13 12:48:41 +0100

    Refactoring Units

--- units/weight_unit.go
+++ units/weight_unit.go
@@ -3,7 +3,7 @@ package units
 const (
 	GRAM 		float64 = 1
 	MILI_GRAM	float64	= GRAM / 1000
-	HECTO_GRA	float64 = GRAM * 100
+	HECTO_GRAM	float64 = GRAM * 100
 	KILO_GRAM 	float64 = GRAM * 1000
 	TONNE 		float64 = KILO_GRAM * 1000
 
