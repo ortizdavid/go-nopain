8902e47d28b9db6780718f056f6180a17e2ae3b2
ortizdavid <ortizaad1994@gmail.com>
2024-03-07 17:08:31 +0100

    Finished StringToNumberOrNil

--- conversion/string.go
+++ conversion/string.go
@@ -62,6 +62,58 @@ func StringToIntOrNil(strNumber string) *int {
 	return &numInt
 }
 
+// string -> int32 or nil
+func StringToInt32OrNil(strNumber string) *int32 {
+	if strNumber == "" {
+		return nil
+	}
+	numInt, err := strconv.ParseInt(strNumber, 10, 32)
+	if err != nil {
+		return nil
+	}
+	result := int32(numInt)
+	return &result
+}
+
+// string -> int64 or nil
+func StringToInt64OrNil(strNumber string) *int64 {
+	if strNumber == "" {
+		return nil
+	}
+	numInt, err := strconv.ParseInt(strNumber, 10, 64)
+	if err != nil {
+		return nil
+	}
+	result := int64(numInt)
+	return &result
+}
+
+// string -> float32 or nil
+func StringToFloat32OrNil(strNumber string) *float32 {
+	if strNumber == "" {
+		return nil
+	}
+	numFloat, err := strconv.ParseFloat(strNumber, 32)
+	if err != nil {
+		return nil
+	}
+	result := float32(numFloat)
+	return &result
+}
+
+// string -> float64 or nil
+func StringToFloat64OrNil(strNumber string) *float64 {
+	if strNumber == "" {
+		return nil
+	}
+	numFloat, err := strconv.ParseFloat(strNumber, 64)
+	if err != nil {
+		return nil
+	}
+	result := float64(numFloat)
+	return &result
+}
+
 // string -> String or Nil
 func StringOrNil(str string) *string {
 	if str == "" {
