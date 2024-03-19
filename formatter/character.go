a72c936e5bc220428f443785a9238043ce07537f
ortizdavid <ortizaad1994@gmail.com>
2024-03-19 13:39:09 +0100

    Implementing Left Pad

---
+++ formatter/character.go
@@ -0,0 +1,23 @@
+package formatter
+
+import "strings"
+
+// Add repeated chars to the left of a string value
+// Returns the concatenation of Padding and Original value
+// Padding: char reptead N times
+func LeftPadWithChar(originalValue string, allowedLenght int, char string) string {
+	originalLength := len(originalValue)
+
+	if allowedLenght <= 0 || char == ""  || originalLength == 0 {
+		return ""
+	}
+
+	if  originalLength >= allowedLenght {
+		return originalValue
+	} 
+
+	paddingLength := allowedLenght - originalLength
+	padding := strings.Repeat(char, paddingLength)
+	return padding + originalValue
+}
+
