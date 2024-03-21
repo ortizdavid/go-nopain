edf6cb5bac471404cc5fc2bced25279121e7fa24
ortizdavid <ortizaad1994@gmail.com>
2024-03-21 09:24:58 +0100

    Implemented: Right Padding with char

--- formatter/padding.go
+++ formatter/padding.go
@@ -21,3 +21,22 @@ func LeftPadWithChar(originalValue string, allowedLenght int, char string) strin
 	return padding + originalValue
 }
 
+
+// Add repeated chars to the left of a string value
+// Returns the concatenation of Padding and Original value
+// Padding: char reptead N times
+func RightPadWithChar(originalValue string, allowedLenght int, char string) string {
+	originalLength := len(originalValue)
+
+	if allowedLenght <= 0 || char == ""  || originalLength == 0 {
+		return ""
+	}
+
+	if  originalLength >= allowedLenght {
+		return originalValue
+	} 
+
+	paddingLength := allowedLenght - originalLength
+	padding := strings.Repeat(char, paddingLength)
+	return originalValue + padding
+}
