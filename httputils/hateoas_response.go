97d58567aab2310efe099ce7089c040da4104d70
ortizdavid <ortizaad1994@gmail.com>
2024-04-09 11:41:58 +0100

    init Hateoas Response

---
+++ httputils/hateoas_response.go
@@ -0,0 +1,48 @@
+package httputils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type hateoasJson struct {
+	Message *string `json:"message,omitempty"`
+	Status int `json:"status"`
+	Count *int `json:"count,omitempty"`
+	Data any `json:"data,omitempty"`
+	links link `json:"links"`
+}
+
+type link struct {
+	Path string `json:"path"`
+	Self string `json:"self"`
+	Rel string `json:"rel,omitempty"`
+}
+
+func WriteHateoasJson(w http.ResponseWriter, r *http.Request, statusCode int, data any, count int) {
+	writeJsonHeader(w, statusCode)
+	
+	basePath := fmt.Sprintf("%s://%s", r.URL.Scheme, r.URL.Host)
+	selfLink := basePath + r.URL.Path
+	
+	response := hateoasJson{
+		Message: new(string),
+		Status:  statusCode,
+		Count:   &count,
+		Data:    data,
+		links:   link{
+			Path: basePath,
+			Self: selfLink,
+		},
+	}
+	encodeHateoas(w, response)
+}
+
+func encodeHateoas(w http.ResponseWriter, response hateoasJson) {
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		fmt.Fprintf(w, "%s", err.Error())
+	}
+}
+
