1715f001ede84cdbdd777d389350b55356c01d53
ortizdavid <ortizaad1994@gmail.com>
2024-04-17 12:26:53 +0100

    refactored: percent funtions

--- calculations/percentage.go
+++ calculations/percentage.go
@@ -4,13 +4,13 @@ const hundred = 100
 
 // CalculatePercentage calculates the percentage of a given value.
 // It takes the original value and the percentage to calculate.
-func CalculatePercentage(originalValue float32, percentage float32) float32 {
+func PercentageOfValue(originalValue float32, percentage float32) float32 {
     return (percentage * originalValue) / hundred
 }
 
 // CalculateValueFromPercentage calculates the original value based on a given percentage and a calculated value.
 // It takes the percentage and the calculated value as parameters.
-func CalculateValueFromPercentage(percentage float32, calculatedValue float32) float32 {
+func ValueFromPercentage(percentage float32, calculatedValue float32) float32 {
     // Formula: originalValue = (calculatedValue * 100) / percentage
     return (calculatedValue * hundred) / percentage
 }
