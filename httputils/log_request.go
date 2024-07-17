d0e1c3e80cc14f8648070cda80a32430fed952cb
ortizdavid <ortizaad1994@gmail.com>
2024-07-17 16:01:17 +0100

    Changed: Logging Middleware

--- httputils/log_request.go
+++ httputils/log_request.go
@@ -5,9 +5,13 @@ import (
 	"net/http"
 )
 
-func LogHttpRequests(handler http.Handler) http.Handler {
+func logRequests(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s", r.Method, r.RequestURI, r.RemoteAddr)
 		handler.ServeHTTP(w, r)
 	})
+}
+
+func UseLoggingMiddleware(mux *http.ServeMux) {
+	logRequests(mux)
 }
