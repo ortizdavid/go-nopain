5d7642cc4b8e3a49cc36f395199ac50406337237
ortizdavid <ortizaad1994@gmail.com>
2024-07-17 15:38:31 +0100

    Changed: log Requests

--- httputils/log_request.go
+++ httputils/log_request.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func LogRequests(handler http.Handler) http.Handler {
+func LogHttpRequests(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s", r.Method, r.RequestURI, r.RemoteAddr)
 		handler.ServeHTTP(w, r)
